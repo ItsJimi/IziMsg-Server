01837d184c045b96e49e060d4ef92e89c9a1133c
Florian HERMOUET-JOSCHT <fhermoue@e2r9p7.42.fr>
2016-05-24 17:56:01 +0200

    Params + color gestion

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: 4242 });
 
+var colors = {
+	turquoise: "78edd5",
+	emerald: "82e3aa",
+	peter_river: "7dbde8",
+	amethyst: "bf95d0",
+	wet_asphalt: "34495e"
+};
+
+var config = {
+	serverName: "IziMsg",
+	colors: colors
+};
+
 wss.on('connection', function(ws) {
-	var username = undefined;
+	ws.username = undefined;
+	ws.color = undefined;
+
+	config.usedColors = getUsedColors(wss);
+
+	ws.send(JSON.stringify(config));
 
 	ws.on('message', function(res) {
 		console.log('received: %s', res);
 		//parse message to JSON
-		var msg = JSON.parse(res);
+		try {
+			var msg = JSON.parse(res);
 
-		if (msg.user) {
-			username = msg.user;
-		} else if (msg.msg) {
-			var send = {};
-			send.user = username;
-			send.msg = msg.msg;
-			wss.broadcast(JSON.stringify(send));
+			if (msg.user) {
+				ws.username = msg.user;
+			}
+			if (msg.color) {
+				if (colors[msg.color]) {
+					ws.color = msg.color;
+				} else {
+					sendError(ws, "color_not_exist");
+				}
+			}
+			if (ws.username && ws.color && msg.msg) {
+				var send = {};
+				send.user = ws.username;
+				send.color = ws.color;
+				send.msg = msg.msg;
+				wss.broadcast(JSON.stringify(send));
+			}
+		} catch (e) {
+			sendError(ws, "Are you idiot?");
+			console.log(e);
 		}
 	});
 
@@ -26,3 +58,19 @@ wss.broadcast = function(data) {
 		client.send(data);
 	});
 };
+
+function getUsedColors(wss) {
+	var used = [];
+	wss.clients.forEach(function(client) {
+		if (used.indexOf(client.color) == -1) {
+			used.push(client.color);
+		}
+	});
+	return used.filter(Boolean);
+}
+
+function sendError(ws, text) {
+	var r = {};
+	r.error = text;
+	ws.send(JSON.stringify(r));
+}
